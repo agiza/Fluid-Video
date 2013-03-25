ba8f5f12f177a57e070c0579825c1ab8d03bb7b8
Kyle Weiner <kyleweiner@Kyle-Weiners-MacBook-Pro.local>
2013-03-25 07:06:56 -0400

    Converted tabs to spaces

--- jquery.fluidvideo.js
+++ jquery.fluidvideo.js
@@ -10,54 +10,54 @@
 
 (function ($) {
 
-	"use strict";
+    "use strict";
 
-	$.fn.fluidVideo = function (options) {
-		var src,
-			width,
-			height,
-			settings,
-			aspectRatio,
-			$video;
+    $.fn.fluidVideo = function (options) {
+        var src,
+            width,
+            height,
+            settings,
+            aspectRatio,
+            $video;
 
-		settings = $.extend({
-			vendors: {},
-			parentClass: "fluid-video-wrapper"
-		}, options);
+        settings = $.extend({
+            vendors: {},
+            parentClass: "fluid-video-wrapper"
+        }, options);
 
-		this.each(function () {
-			$video = $(this);
+        this.each(function () {
+            $video = $(this);
 
-			// Determine the video's aspect ratio
+            // Determine the video's aspect ratio
 
-			width = $video.attr("width");
-			height = $video.attr("height");
-			aspectRatio = height / width;
+            width = $video.attr("width");
+            height = $video.attr("height");
+            aspectRatio = height / width;
 
-			/**
-			 * Remove the video's width and height attributes,
-			 * wrap it in a div and set its aspect ratio
-			 */
+            /**
+             * Remove the video's width and height attributes,
+             * wrap it in a div and set its aspect ratio
+             */
 
-			$video
-				.removeAttr("width")
-				.removeAttr("height")
-				.wrap('<div class="' + settings.parentClass + '" />')
-				.parent('.' + settings.parentClass)
-				.css("padding-top", (aspectRatio * 100) + "%");
+            $video
+                .removeAttr("width")
+                .removeAttr("height")
+                .wrap('<div class="' + settings.parentClass + '" />')
+                .parent('.' + settings.parentClass)
+                .css("padding-top", (aspectRatio * 100) + "%");
 
-			// Run the correct callback for the video based on its vendor
+            // Run the correct callback for the video based on its vendor
 
-			if (settings.vendors) {
-				src = $video.attr("src").toLowerCase();
+            if (settings.vendors) {
+                src = $video.attr("src").toLowerCase();
 
-				$.each(settings.vendors, function (k) {
-					if ((src.indexOf(k.toLowerCase()) > -1) && settings.vendors[k].callback) {
-						settings.vendors[k].callback($video, src);
-					}
-				});
-			}
-		});
-	};
+                $.each(settings.vendors, function (k) {
+                    if ((src.indexOf(k.toLowerCase()) > -1) && settings.vendors[k].callback) {
+                        settings.vendors[k].callback($video, src);
+                    }
+                });
+            }
+        });
+    };
 
 }(jQuery));
